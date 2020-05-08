8fc4dcbd236e4030167c9ef56c7e8e7f94abfe3d
Robert A <robert.acsinte@gmail.com>
2020-05-08 09:26:34 +0200

    you can get the current location and show it on the map

---
+++ app/src/main/java/com/example/location/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.location;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap gm;
+    private Marker marker;
+    LatLng cord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = new SupportMapFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+        mapFragment.getMapAsync(this);
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        cord = new LatLng(51.4416, 5.4697);
+        gm = googleMap;
+        marker = gm.addMarker(new MarkerOptions().position(cord)
+                .title("Test Marker"));
+        gm.moveCamera(CameraUpdateFactory.newLatLng(cord));
+    }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(marker != null){
+            cord = new LatLng(location.getLatitude(), location.getLongitude());
+            marker.setPosition(cord);
+            gm.moveCamera(CameraUpdateFactory.newLatLng(cord));
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
