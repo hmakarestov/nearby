c332558b4d4d73e11528fa6b29c42cf8d568f9ee
Marc Dane <i387400@student.fontys.nl>
2020-05-11 12:28:27 +0200

    added email registration and login + check if user is logged in

---
+++ app/src/main/java/fhict/nl/nearby/LoginActivity.java
@@ -0,0 +1,147 @@
+package fhict.nl.nearby;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail;
+    EditText etPassword;
+    Button btnEmailLogin;
+    Button btnLaunchRegister;
+    final int LAUNCH_REGISTER_ACTIVITY = 1;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        setContentView(R.layout.activity_login);
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        //UI
+        etEmail = findViewById(R.id.editTextLoginEmail);
+        etPassword = findViewById(R.id.editTextLoginPassword);
+        btnEmailLogin = findViewById(R.id.btnLoginEmail);
+        btnEmailLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EmailLogin();
+            }
+        });
+
+        final Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        btnLaunchRegister = findViewById(R.id.btnLaunchRegister);
+        btnLaunchRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(intent, LAUNCH_REGISTER_ACTIVITY);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+       //prevent going back if not logged in
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == LAUNCH_REGISTER_ACTIVITY) {
+            if(resultCode == Activity.RESULT_OK){
+                //user is created and is logged in
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //user pressed backbutton
+            }
+        }
+    }
+
+    private void EmailLogin(){
+        etEmail.setError(null);
+        etPassword.setError(null);
+
+        if(!ValidForm()){
+            return;
+        }
+
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //login success
+                    setResult(Activity.RESULT_OK);
+                    finish();
+                }else{
+                    Exception e = task.getException();
+                    if(e instanceof FirebaseAuthInvalidUserException){
+                        Toast.makeText(LoginActivity.this, "Email was not found.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        if(e instanceof FirebaseAuthInvalidCredentialsException){
+                            Toast.makeText(LoginActivity.this, "Password was wrong.", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "Unknown error occurred. Please try again later", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean ValidForm(){
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if(email.isEmpty() || password.isEmpty()){
+            if(email.isEmpty()){
+                etEmail.setError("Required");
+            }
+
+            if(password.isEmpty()){
+                etPassword.setError("Required");
+            }
+            return false;
+        }
+
+        if(!email.matches(emailPattern)){
+            etEmail.setError("Email does not contain a valid format.");
+            return false;
+        }
+
+        if(password.length() < 8){
+            etPassword.setError("Password should be 8 characters or more.");
+            return false;
+        }
+
+        return true;
+    }
+}
