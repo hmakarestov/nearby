1d4976dd813762bf3773ef7746a7ec7c8d7e70b1
Hristo Makarestov <hristo.makarestov@gmail.com>
2020-06-13 18:16:35 +0300

    Created logout option and updated the design

--- app/src/main/java/fhict/nl/nearby/MenuActivity.java
+++ app/src/main/java/fhict/nl/nearby/MenuActivity.java
@@ -1,7 +1,9 @@
 package fhict.nl.nearby;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -91,6 +93,19 @@ public class MenuActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_logout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this,MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
